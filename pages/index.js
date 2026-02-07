8ef042df69db24169519eaccc1fb4d15eceb782a
laieth772-dotcom <laieth772@gmail.com>
2026-02-08 00:55:58 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20, direction: 'rtl', fontFamily: 'sans-serif' }}>
+      <h1>🎉 الموقع اشتغل بنجاح</h1>
+      <p>هذا أول موقع Next.js شغال من الموبايل</p>
+      <p>إدارة الدواجن</p>
+    </div>
+  )
+}
